826b9112172b580fae7a2fed2612b46d4b740885
24hs115 <24hs115@s.bgu.ac.jp>
2024-12-02 14:17:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
         function checkCode() {
-            var number1 = document.getElementById('number-select1').value;
-            var number2 = document.getElementById('number-select2').value;
-            var number3 = document.getElementById('number-select3').value;
-            var number4 = document.getElementById('number-select4').value;
+            var number1 = document.getElementById('number-select1');
+            var number2 = document.getElementById('number-select2');
+            var number3 = document.getElementById('number-select3');
+            var number4 = document.getElementById('number-select4');
             var result = "開かない"; // デフォルトの値
 
             if (number1 === "0" && number2 === "0" && number3 === "0" && number4 === "0") {
