2894af16778bbb8940d6ec0e3af0f1cbe139bce6
24hs115 <24hs115@s.bgu.ac.jp>
2024-12-09 14:04:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,21 @@
             var number4 = document.getElementById('number_select4').value;
             var result = "開かない"; // デフォルトの値
 
-            if (number1 === "0" && number2 === "0" && number3 === "0" && number4 === "0") {
+            if ((number1 === "0" && number2 === "0") && (number3 === "0" && number4 === "0")) {
                 result = "開いた　これは本当にやめたほうがいいね";
-            }else if(number1 === "0" && number2 === "0" && number3 === "0" && number4 === "1") {
+            }else if((number1 === "0" && number2 === "0") && (number3 === "0" && number4 === "1")) {
                 result = "まさか1から試そうとしてないよね？"; 
-            }else if(number1 === "1" && number2 === "2" && number3 === "3" && number4 === "4") {
+            }else if((number1 === "1" && number2 === "2") && (number3 === "3" && number4 === "4")) {
                 result = "開いた　覚えやすいよね"; 
-            }else if(number1 === "2" && number2 === "5" && number3 === "2" && number4 === "5") {
+            }else if((number1 === "2" && number2 === "5") && (number3 === "2" && number4 === "5")) {
                 result = "開いた　結構な人が使ってると思う"; 
-            }else if(number1 === "1" && number2 === "1" && number3 === "2" && number4 === "9") {
+            }else if((number1 === "1" && number2 === "1") && (number3 === "2" && number4 === "9")){
                 result = "開いた　お肉好きなんだね"; 
-            }else if(number1 === "3" && number2 === "1" && number3 === "5" && number4 === "0") {
+            }else if((number1 === "3" && number2 === "1") && (number3 === "5" && number4 === "0")) {
                 result = "開いた　サイコー！"; 
-            }else if(number1 === "5" && number2 === "9" && number3 === "6" && number4 === "3") {
+            }else if((number1 === "5" && number2 === "9") && (number3 === "6" && number4 === "3")){
                 result = "開いた　大声で叫ばないようにね"; 
-            }else if(number1 === "7" && number2 === "7" && number3 === "7" && number4 === "7") {
+            }else if((number1 === "7" && number2 === "7") && (number3 === "7" && number4 === "7")){
                 result = "開いた　運がよくなりそうだね"; 
             }
             else{
